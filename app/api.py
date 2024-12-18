14803b81762915fc72ab1dd02b9dd14cd3f671a9
sant8pool <43548198+sant8pool@users.noreply.github.com>
2024-12-18 15:29:51 +0100

    Update api.py substract

--- app/api.py
+++ app/api.py
@@ -24,11 +24,11 @@ def add(op_1, op_2):
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
 
-@api_application.route("/calc/substract/<op_1>/<op_2>", methods=["GET"])
+@api_application.route("/calc/subtract/<op_1>/<op_2>", methods=["GET"])
 def subtract(op_1, op_2):
     try:
         num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
-        return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
+        return ("{}".format(CALCULATOR.subtract(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
